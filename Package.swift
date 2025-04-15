1de2e1add3127fdd61475effd160b3087b828160
alephao <7674479+alephao@users.noreply.github.com>
2025-04-15 11:13:05 -0300

    fix: update swift-snapshot-testing

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0"),
     .package(url: "https://github.com/apple/swift-crypto.git", from: "3.0.0"),
-    .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.12.0"),
+    .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.18.0"),
   ],
   targets: [
     .executableTarget(
